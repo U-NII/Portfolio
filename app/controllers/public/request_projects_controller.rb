43e99738a070164f806ac32f494c509df6e5fde7
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-06-26 11:03:08 +0000

    [Up date ]

--- app/controllers/public/request_projects_controller.rb
+++
@@ -1,9 +0,0 @@
-class Public::RequestProjectsController < ApplicationController
-  belongs_to :request
-  belongs_to :project
-
-  validates :project_id, :request_id, :quantity, :price, presence: true
-  validates :price, :quantity, numericality: { only_integer: true }
-
-  enum project_status: {"着手不可": 0,"制作待ち": 1,"製作中": 2,"制作完了": 3 }
-end
