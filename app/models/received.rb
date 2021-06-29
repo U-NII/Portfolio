fd273f6c73b92c0cc0e074c345802d07bfaa3787
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-06-29 14:08:33 +0000

    [Up date]

--- app/models/received.rb
+++ app/models/received.rb
@@ -1,18 +1,18 @@
 class Received < ApplicationRecord
   belongs_to :member
-  
+
   validates :member_id, :name, presence: true
   validates :telephone_number, length: {is: 11}, numericality: { only_integer: true }
   validate :blank_name
   validate :blank_telephone_number
-  
+
   def request_address
     [name,telephone_number].join(' ')
   end
-  
+
   def blank_name
     return if entrys_option == 1
-    
+
     if name.blank?
       errors.add(:name, 'は入力する必要があります')
     end
@@ -20,9 +20,9 @@ class Received < ApplicationRecord
 
   def blank_telephone_number
     return if entrys_option == 2
-    
+
     if telephone_number.blank?
-      errors.add(:telephone_number, "は入力する必要があります。")
+      errors.add(:telephone_number, "は入力する必要があります")
     end
   end
 end
